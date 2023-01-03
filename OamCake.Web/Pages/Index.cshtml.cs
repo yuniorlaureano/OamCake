c45775502123ae6f7cc85a27269fb0045d1b230c
Oam <yuniorlaureano@gmail.com>
2023-01-02 22:58:27 -0400

    add list of catalog on index page add serach capability on index page

--- OamCake.Web/Pages/Index.cshtml.cs
+++ OamCake.Web/Pages/Index.cshtml.cs
@@ -1,20 +1,43 @@
 ﻿using Microsoft.AspNetCore.Mvc;
 using Microsoft.AspNetCore.Mvc.RazorPages;
+using Microsoft.EntityFrameworkCore;
+using OamCake.Data;
+using OamCake.Data.Dto;
+using OamCake.Entity;
 
 namespace OamCake.Web.Pages
 {
     public class IndexModel : PageModel
     {
-        private readonly ILogger<IndexModel> _logger;
+        private readonly OamCakeContext _db;
+        public Catalog Catalog { get; set; } = new();
+        public IEnumerable<Entity.Category> Categories { get; set; } = Enumerable.Empty<Entity.Category>();
 
-        public IndexModel(ILogger<IndexModel> logger)
+        public IndexModel(OamCakeContext db)
         {
-            _logger = logger;
+            _db = db;
         }
 
-        public void OnGet()
-        {
+        [BindProperty(SupportsGet = true)]
+        public int Pages { get; set; }
 
+        [BindProperty(SupportsGet = true)]
+        public string? Search { get; set; }
+
+        [BindProperty(SupportsGet = true)]
+        public int? CategoryId { get; set; }
+
+        public async Task OnGet()
+        {
+            var query = _db.Catalog.Include(x => x.CatalogDetails)
+                                   .ThenInclude(x => x.Cake).Where(x => x.DeletedAt == null && x.IsPublished);
+
+            Catalog = await query.FirstOrDefaultAsync();
+            Catalog.CatalogDetails = Catalog.CatalogDetails.Where(x => 
+                (x.Cake.CategoryId == CategoryId || CategoryId == null) &&
+                (Search == null || x.Cake.Name.Contains(Search, StringComparison.OrdinalIgnoreCase))
+            ).ToList();
+            Categories = await _db.Category.ToListAsync();
         }
     }
 }
